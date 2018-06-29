3cee43149abab63686f5e94426963d85cf71cad4
Rok Povsic <rok.povsic@gmail.com>
2018-06-29 22:09:47 +0200

    LOCALE set.

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,6 +11,7 @@ PATH = 'content'
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
+LOCALE = ('en_US',)
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
